c4ac302f38b4806e5700d5e9b57a6ae4bae28113
Saleem Shaik <shaiksa@cdkglobal.com>
2021-01-19 16:46:22 +0530

    first commit

---
+++ src/components/Result.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function Result({ result, openPopup }) {
+	return (
+		<div className="result" onClick={() => openPopup(result.imdbID)}>
+			<img alt = {"poster"} src={result.Poster} />
+			<h3>{result.Title}</h3>
+		</div>
+	)
+}
+
+export default Result;
